ec38a9ce9602acc1ea5bcc5311f95ca10bd4744d
TibScript <thibaut.gounouf@gmail.com>
2021-03-06 08:46:14 +0100

    Update front to display tree
    Init list module to use
    Init link with back

--- src/front/src/app/app.component.ts
+++ src/front/src/app/app.component.ts
@@ -1,4 +1,42 @@
+import {NestedTreeControl} from '@angular/cdk/tree';
 import { Component } from '@angular/core';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+import { from } from 'rxjs';
+
+import { Tree } from './tree'
+import { TreeService } from './tree.service'
+
+const TREE_DATA: Tree[] = [
+  {
+    uid:0,
+    name: 'Fruit',
+    children: [
+      {uid:1,name: 'Apple'},
+      {uid:2,name: 'Banana'},
+      {uid:3,name: 'Fruit loops'},
+    ]
+  }, {
+    uid:4,
+    name: 'Vegetables',
+    children: [
+      {
+        uid:5,
+        name: 'Green',
+        children: [
+          {uid:6,name: 'Broccoli'},
+          {uid:7,name: 'Brussels sprouts'},
+        ]
+      }, {
+        uid:8,
+        name: 'Orange',
+        children: [
+          {uid:9,name: 'Pumpkins'},
+          {uid:10,name: 'Carrots'},
+        ]
+      },
+    ]
+  },
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +44,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'front';
+  title = 'Team Board';
+  treeControl = new NestedTreeControl<Tree>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<Tree>();
+
+  constructor(
+    private treeService: TreeService
+  ) {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: Tree) => !!node.children && node.children.length > 0;
 }
--- src/front/src/app/app.module.ts
+++ src/front/src/app/app.module.ts
@@ -1,5 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatTreeModule } from '@angular/material/tree'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +27,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
